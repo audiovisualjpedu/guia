d99c13590c45d35d5c0c1c95eb7bbfe2e843ab99
audiovisualjpedu <112101037+audiovisualjpedu@users.noreply.github.com>
2023-12-12 14:47:26 -0300

    Corrigido o botao Pagina Inicial da pagina secundaria

--- assets/js/btnscript.js
+++ assets/js/btnscript.js
@@ -1,3 +1,12 @@
+// Função para tornar o botão visível após um atraso de 3 segundos
+function showFloatingButton() {
+    var floatingButton = document.getElementById('floatingButton');
+    floatingButton.style.display = 'block';
+}
+
+// Atraso de 3 segundos antes de chamar a função para tornar o botão visível
+setTimeout(showFloatingButton, 3000);
+
 function redirectToHome() {
     // Substitua 'pagina_inicial.html' pela URL da sua página inicial
     window.location.href = 'index.html';
